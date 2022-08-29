543ab38c056d40e863363caf462b17f563c83e9c
Hamza Ali <hamzamughal2145@gmail.com>
2022-08-29 12:39:52 -0400

    ArrayListReview

--- 02.CollectionsReview/src/main/java/ArrayListReview.java
+++ 02.CollectionsReview/src/main/java/ArrayListReview.java
@@ -4,12 +4,21 @@ public class ArrayListReview {
     public static void main(String[] args) {
         // Create ArrayList and a class
 
+        List<Student> students = new ArrayList<>();
 
-// Add elements to ArrayList
+        // Add elements to ArrayList
+        students.add(new Student(1, "Jack"));
+        students.add(new Student(2, "Julia"));
+        students.add(new Student(3, "Mike"));
+        students.add(new Student(4, "Mary"));
 
 
         // Iteration on ArrayLists
         // 1. For Loop with get(index)
+        System.out.println("Printing with for loop");
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i));
+        }
 
         // 2. Iterator
         // Forward Iteration
