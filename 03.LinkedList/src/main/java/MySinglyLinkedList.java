0c6768d7e7a69b596df6f0d6c036569e7ab34cb6
Hamza Ali <hamzamughal2145@gmail.com>
2022-09-09 15:39:16 -0400

    Deletion method added SinglyLinkedList

--- 03.LinkedList/src/main/java/MySinglyLinkedList.java
+++ 03.LinkedList/src/main/java/MySinglyLinkedList.java
@@ -30,4 +30,38 @@ public class MySinglyLinkedList {
             current = current.next;
         }
     }
+
+    void deleteById(int id) {
+        //check if empty
+        if (isEmpty()) System.out.println("List is empty");
+        //assign prev and current with head
+        Node prev = head;
+        Node current = head;
+        while (current != null) {
+            if (current.id == id) {//there is match
+                //case 1: head
+                if (current == head) {
+                    head = current.next;
+                    current.next = null;
+                }
+                //case 2: tail
+                if (current == tail) {
+                    tail = prev;
+                    prev.next = null; // tail will be eligible for GC
+                }
+                //case 3: middle
+                else {
+                    prev.next = current.next;
+                    current.next = null; // current will be eligible for GC
+                }
+                // after deletion
+                size--;
+            }
+            // move forward on the elements of the list
+            prev = current;
+            current = current.next;
+        }
+    }
+
+
 }
