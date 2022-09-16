ba65717472ca337640f04df2db77c22bd6057af9
Hamza Ali <hamzamughal2145@gmail.com>
2022-09-16 13:11:25 -0400

    Stack

---
+++ 05.Stacks/src/main/java/StackApp.java
@@ -0,0 +1,16 @@
+public class StackApp {
+    public static void main(String[] args) {
+
+        MyStack<Integer> stack = new MyStack<>();
+
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+        }
+        System.out.println(stack.peek());
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(stack.pop());
+        }
+
+    }
+}
