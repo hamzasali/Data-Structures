bee689b3c1e9eed29eb057c5fac3daf33f8ca5de
Hamza Ali <hamzamughal2145@gmail.com>
2022-09-22 12:04:28 -0400

    Stacks

--- 05.Stacks/src/main/java/SunsetViews.java
+++ 05.Stacks/src/main/java/SunsetViews.java
@@ -1,16 +1,60 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
 
 public class SunsetViews {
     public static void main(String[] args) {
         int[] buildings = {3, 5, 4, 4, 3, 1, 3, 2};
-        sunsetViews(buildings, "EAST").forEach(System.out::println);
+        sunsetViews(buildings, "west").forEach(System.out::println);
     }
 
     public static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
-        // Write your code here.
-        // Write your code here.
-        // Write your code here.
-        return new ArrayList<Integer>();
+        Stack<Integer> stack = new Stack<>();
+        //this is for east side
+        int i = 0;
+        int step = 1;
+
+        //this is for west side
+        if (direction.equalsIgnoreCase("west")) {
+            i = buildings.length - 1;
+            step = -1;
+        }
+        while (i >= 0 && i < buildings.length) {
+
+            while (!stack.isEmpty() && buildings[i] >= buildings[stack.peek()]) {
+                stack.pop();
+
+            }
+            stack.push(i);
+            i += step;
+        }
+
+        if (direction.equalsIgnoreCase("west")) Collections.reverse(stack);
+
+        return new ArrayList<>(stack);
+
+        /*Stack<Integer> stack = new Stack<>();
+
+        if (direction.equalsIgnoreCase("east")) {
+            for (int i = 0; i < buildings.length; i++) {
+                while (!stack.isEmpty() && buildings[i] >= buildings[stack.peek()]) {
+                    stack.pop();
+                }
+                stack.push(i);
+            }
+        } else {
+            for (int i = buildings.length - 1; i >= 0; i--) {
+                while (!stack.isEmpty() && buildings[i] >= buildings[stack.peek()]) {
+                    stack.pop();
+                }
+                stack.push(i);
+            }
+        }
+        Collections.reverse(stack);
+        return new ArrayList<>(stack);*/
+
+
     }
 }
 /**
