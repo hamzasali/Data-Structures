7edb67ae3c3f300f839fd54c706b01861ef54fd4
Hamza Ali <hamzamughal2145@gmail.com>
2022-11-18 11:49:10 -0500

    Tree node sum method added

--- 08.Trees/src/main/java/TreeApp.java
+++ 08.Trees/src/main/java/TreeApp.java
@@ -2,8 +2,8 @@ public class TreeApp {
     public static void main(String[] args) {
 
         MyTree tree = new MyTree();
-        int[] numbers = new int[]{10, 5, 12, 3, 6, 11, 13, 2};
-        for (int i = 0; i < 8; i++) {
+        int[] numbers = new int[]{10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
+        for (int i = 0; i < 11; i++) {
             tree.insert(numbers[i]);
         }
         VisualizeTree.printTree(tree.root, null, false);
@@ -25,5 +25,9 @@ public class TreeApp {
         System.out.println("Number of leaves:  " +tree.countLeaves(tree.root));
         System.out.println("Sum of Leaf Nodes: " + tree.findSumOfLeaves(tree.root));
         System.out.println(tree.height(tree.root));
+
+        System.out.println("Sum of Node Depth "+tree.calculateNodeDepthSums());
+        System.out.println("Sum of Node "+tree.calculateNodeSums());
+
     }
 }
