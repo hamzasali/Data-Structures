3bd42ad3928c5668ddc40f87d70d4aa2bd372aa5
Hamza Ali <hamzamughal2145@gmail.com>
2022-11-18 16:58:20 -0500

    Heap

---
+++ 09.Heap/src/main/java/MyHeap.java
@@ -0,0 +1,80 @@
+import java.util.NoSuchElementException;
+
+public class MyHeap {
+    int[] items;
+    int size;
+
+    public MyHeap(int capacity) {
+        this.items = new int[capacity];
+        this.size = 0;
+    }
+
+    public int peek() {
+        if (size == 0) throw new NoSuchElementException();
+        return items[0];
+    }
+
+    public void insert(int value) {
+        if (size == items.length) throw new IllegalStateException();
+        else {
+            items[size++] = value;
+            bubbleUp();
+        }
+    }
+
+    public void bubbleUp() {
+        //calculate start point/index
+        int index = size - 1;
+        // while index >0 %% parent is less than child continue...
+        // 1. swap child and parent
+        // calculate next index = parent index
+        while (index > 0 && items[index] > items[parentIndex(index)]) {
+            swap(index, parentIndex(index));
+            // assign index with the parent index
+            index = parentIndex(index);//like current=current.next
+        }
+    }
+
+    public void swap(int first, int second) {
+        int temp = items[first];
+        items[first] = items[second];
+        items[second] = temp;
+    }
+
+    public int parentIndex(int index) {
+        return (index - 1) / 2;
+    }
+
+    public boolean hasLeftChild(int index) {
+        return leftChildIndex(index) <= size;
+    }
+
+    public boolean hasRightChild(int index) {
+        return rightChildIndex(index) <= size;
+    }
+
+    public int leftChildIndex(int index) {
+        return index * 2 + 1;
+    }
+
+    public int rightChildIndex(int index) {
+        return index * 2 + 2;
+    }
+
+    public int largerChildIndex(int index) {
+        if (!hasLeftChild(index)) return index;
+        else if (!hasRightChild(index)) {
+            return leftChildIndex(index);
+        }
+        return items[leftChildIndex(index)] > items[rightChildIndex(index)] ? leftChildIndex(index) : rightChildIndex(index);
+    }
+
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i] + ", ");
+        }
+        System.out.println();
+    }
+
+
+}
